980536997762fd6586a303e4a8e14e4a5db73172
Itu <sebastianitu61@yahoo.com>
2023-02-01 16:58:05 +0200

    Adjusted styling for responsive design

--- src/assets/Components/Navigation/Navigation.jsx
+++ src/assets/Components/Navigation/Navigation.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react"
 import { Link, Outlet } from "react-router-dom"
-import styles from './Navigation.module.css'
+import './Navigation.css'
 
 const Navigation = ({data, handleClickedLink}) => {
+
+    const [menuIsToggled, setMenuIsToggled] = useState(false)
+
+    const handleToggle = () => { setMenuIsToggled(!menuIsToggled) }
+
     return(
         <div className="navigation flex flex-col mx-10">
-            <nav className="flex w-full justify-around my-2 mx-auto">
-                <div><Link to='logo'><img src="./logo.png" className="w-32" alt="LOGO" /></Link></div>
-                <ul className="nav-buttons w-full flex justify-between mx-20">
-                    <li className={`text-lg my-auto ${styles.text}`}><Link to='home' onClick={handleClickedLink}> HOME </Link></li>
-                    <li className={`text-lg my-auto ${styles.text}`}><Link to='watchlist' onClick={handleClickedLink}> WATCHLIST </Link></li>
-                    <li className={`text-lg my-auto ${styles.text}`}><Link to='watched' onClick={handleClickedLink}> WATCHED </Link></li>
-                    <li className={`text-lg my-auto ${styles.text}`}><Link to='favorites' onClick={handleClickedLink}> FAVORITES </Link></li>
+            <nav className="mainNavigation flex w-full justify-around my-2 mx-auto">
+                <Link to='logo'><img src="./logo.png" className={`logo w-32`} alt="LOGO" /></Link>
+                <ul className={ menuIsToggled ? 'navButtons show' : 'navButtons hidden' }>
+                    <li className={`text-lg my-auto text`}><Link to='home' onClick={handleClickedLink}> HOME </Link></li>
+                    <li className={`text-lg my-auto text`}><Link to='watchlist' onClick={handleClickedLink}> WATCHLIST </Link></li>
+                    <li className={`text-lg my-auto text`}><Link to='watched' onClick={handleClickedLink}> WATCHED </Link></li>
+                    <li className={`text-lg my-auto text`}><Link to='favorites' onClick={handleClickedLink}> FAVORITES </Link></li>
                 </ul>
+                <img className={`burger`} src="menu-burger.png" alt="MENU" onClick={handleToggle}/>
             </nav>
             <Outlet context={data}/>
         </div>
