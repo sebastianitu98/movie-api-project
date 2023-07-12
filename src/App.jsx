00ef6fb1a86428ba6ac91cde1b76b52491a4970e
Itu <sebastianitu61@yahoo.com>
2023-07-12 14:57:18 +0300

    resolved navigation not displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
 import { useEffect, useState } from 'react'
-import  useToggle  from './hooks/useToggle'
+import useToggle from './hooks/useToggle'
 import Navigation from './assets/Components/Navigation/Navigation'
 import Description from './assets/Components/Description/Description'
 import styles from './App.module.css'
 import { localStorageService } from './utils/localStorageService'
 
 function App() {
-  
+
   const [mostPopular, setMostPopular] = useState()
   const [isToggled, setToggle] = useToggle()
 
   //initialize data from localStorage
   localStorageService.initializeData();
-  
+
   useEffect(() => {
-      fetch(`${import.meta.env.VITE_API_URL}/trending/movie/day?api_key=${import.meta.env.VITE_API_KEY}`)
+    fetch(`${import.meta.env.VITE_API_URL}/trending/movie/day?api_key=${import.meta.env.VITE_API_KEY}`)
       .then(res => res.json())
       .then(res => setMostPopular(res))
-  },[])
+  }, [])
 
   const handleToggle = () => {
-    if (isToggled == true){
+    if (isToggled == true) {
       setToggle()
     }
   }
 
 
   return (
-    <div className={`${styles.App}`}>
-      {mostPopular && <Navigation data={mostPopular} handleClickedLink={handleToggle}/>}
-      <Description isToggled={isToggled} />
-    </div>
+
+    <>
+      {mostPopular &&
+        <div className={`${styles.App}`}>
+          {mostPopular && <Navigation data={mostPopular} handleClickedLink={handleToggle} />}
+          <Description isToggled={isToggled} />
+        </div>
+      }
+    </>
+
   )
 }
 
